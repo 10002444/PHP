412e0913478fb6054f64e326647ee122416e49f1
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-04-09 00:06:04 +1200

    php intial commit

---
+++ Program.php
@@ -0,0 +1,43 @@
+﻿<? php
+
+    $name = "Bob";
+    echo $name;
+
+    echo"<br><br>";
+
+    $age = 64;
+    echo $age;
+
+    $name2 = "Martha";
+    $age2 = 60;
+
+    echo"<br><br>";
+
+    if($age == $age3) {
+        echo "Bob and Martha are the same age";
+    }
+
+// "// checks for value only, /// checks for values and data type e.g. string"
+
+
+    $fruits = ["apple", "banana", "coconut", "strawberry"];
+
+    foreach($fruits as $fruit) {
+        echo $fruit."<br>";
+    }
+
+    function greeting()
+{
+
+    return $name;
+}
+
+    $myname = greeting("Marisa");
+
+echo $myname;
+
+    echo "<br><br>";
+
+    echo gettype($myname);
+
+?>
---
+++ animals/Program.php
@@ -0,0 +1,21 @@
+﻿<? php
+    $animals = ["Dog", "Cat", "Bunny", "Tiger", "Kiwi", "Horse", "Cow", "Lion", "Kangaroo", "Bird", "Dingo", "Elephant", "Shark", "Dolphin", "Giraffe", "Emu", "Crocodile", "Goat", "Monkey", "Lemur"];
+    
+    $output = "";
+
+    function displayDropDown($array, $id)
+{
+        $output.= "<select id=\"{$id}\" >\n";
+    foreach ($array as $animals) {
+            $output.= "\t<option value=\"{$animals}\">{$animals}</option>\n";
+    }
+        $output.= "</select>\n";
+
+    return $output;
+}
+
+echo displayDropDown($animals, "fruit1");
+echo displayDropDown($animals, "fruit2");
+echo displayDropDown($animals, "fruit3");
+
+?>
